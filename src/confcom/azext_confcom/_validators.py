64b9692b921b739444866e3611953611d6378f13
sethho <sethho@microsoft.com>
2024-09-19 10:44:45 -0400

    offloading error checking and updating tests

--- src/confcom/azext_confcom/_validators.py
+++ src/confcom/azext_confcom/_validators.py
@@ -3,20 +3,48 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
+from knack.util import CLIError
 
-def example_name_or_id_validator(cmd, namespace):
-    # Example of a storage account name or ID validator.
-    # pylint: disable=line-too-long
-    # See: https://github.com/Azure/azure-cli/blob/dev/doc/authoring_command_modules/authoring_commands.md#supporting-name-or-id-parameters
-    from azure.cli.core.commands.client_factory import get_subscription_id
-    from msrestazure.tools import is_valid_resource_id, resource_id
 
-    if namespace.storage_account:
-        if not is_valid_resource_id(namespace.RESOURCE):
-            namespace.storage_account = resource_id(
-                subscription=get_subscription_id(cmd.cli_ctx),
-                resource_group=namespace.resource_group_name,
-                namespace="Microsoft.Storage",
-                type="storageAccounts",
-                name=namespace.storage_account,
-            )
+def validate_params_file(namespace):
+    if namespace.arm_template_parameters and not namespace.arm_template:
+        raise CLIError(
+            "Can only use ARM Template Parameters if ARM Template is also present"
+        )
+
+
+def validate_diff(namespace):
+    if (namespace.diff and namespace.input_path) or (namespace.diff and namespace.image_name):
+        raise CLIError("Can only diff CCE policy from ARM Template or YAML File")
+
+
+def validate_print_format(namespace):
+    if sum(map(bool, [namespace.print_policy_to_terminal, namespace.outraw, namespace.outraw_pretty_print])) > 1:
+        raise CLIError("Can only print in one format at a time")
+
+
+def validate_aci_source(namespace):
+    if sum(map(bool, [
+        namespace.input_path,
+        namespace.arm_template,
+        namespace.image_name,
+        namespace.virtual_node_yaml_path
+    ])) != 1:
+        raise CLIError("Can only generate CCE policy from one source at a time")
+
+
+def validate_faster_hashing(namespace):
+    if namespace.faster_hashing and namespace.tar_mapping_location:
+        raise CLIError("Cannot use --faster-hashing with --tar")
+
+
+def validate_save_to_file(namespace):
+    if namespace.save_to_file and namespace.arm_template and not (
+        namespace.print_policy_to_terminal or namespace.outraw or namespace.outraw_pretty_print
+    ):
+        raise CLIError("Must print policy to terminal when saving to file")
+
+
+def validate_katapolicygen_input(namespace):
+    if namespace.yaml_path and not namespace.print_version:
+        raise CLIError("Either --yaml-path or --print-version is required")
